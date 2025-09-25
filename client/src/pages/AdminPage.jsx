490ae1386295a9fb64219cc921fd6ec9219b7ec6
kazimayaan <kazimayaan486@gmail.com>
2025-09-26 03:26:32 +0530

    env variables

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Supabase client
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default function AdminPage() {
